0d7cbae5ee6049ac3940504409d5f79fe73485c7
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-11-15 16:06:56 -0300

    add arquivos iniciais

---
+++ src/main/java/br/com/apiindicalocal/APIIndicatesLocation/ApiIndicatesLocationApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiindicalocal.APIIndicatesLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIndicatesLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIndicatesLocationApplication.class, args);
+	}
+
+}
